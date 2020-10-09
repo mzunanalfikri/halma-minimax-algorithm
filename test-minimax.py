48af809f04e687c4d72888fdbb5223b03b6c0f2c
mzunanalfikri <zunanalfikri@gmail.com>
2020-10-09 19:33:52 +0700

    test-minimax

--- test-minimax.py
+++ test-minimax.py
@@ -2,6 +2,10 @@ import random
 
 MAX_DEPTH = 3
 
+'''
+input state
+return state (best move)
+'''
 def minimax_decision(state):
     generated_state = possible_state(state)
     print(generated_state)
@@ -13,11 +17,12 @@ def minimax_decision(state):
         if  temp_obj_value > max_obj_value :
             max_obj_value = temp_obj_value
             best_state_idx = i
-    return max_obj_value
-    # return generated_state[best_state_idx] # harusnya ini yang dipakai
+    # return max_obj_value
+    return generated_state[best_state_idx] # harusnya ini yang dipakai
 
 
 def possible_state(state):
+    #iterasi semua pion, pakai fungsi generate_all_move
     return [random.random() for i in range(2)]
 
 def min_value(state, depth):
@@ -46,5 +51,29 @@ def check_win_state(state):
 def objective_func(state):
     return random.random()
 
+# ini masih bayangan
+def generate_all_move(state, positon):
+    pos_move = [] #hasil
+    queue = []
+    expand = []
+    #generate yang satu step
+    pos_move.append(generate_one_step(state, positon))
+    #inisiasi gerakan yang lompat
+    init_jump_step = generate_jump_step(state, position)
+    pos_move.append(init_jump_step)
+    queue.append(init_jump_step)
+    while len(queue) != 0 :
+        temp_move = queue.pop()
+        if temp_move not in expand: #kalo belom pernah di expand, expand!
+            expand.append(temp_move)
+            #generate semua langkah loncat yang mungkin dari yang di expand
+            temp_jump_move = generate_jump_step(state, temp_move)
+            for move in temp_jump_move:
+                if move not in pos_move: 
+                    pos_move.append(move)
+                if move not in queue: # masukin ke queue buat generate lagi nanti
+                    queue.append(move)
+    return pos_move
+            
 if __name__ == "__main__":
     print(minimax_decision(4))
